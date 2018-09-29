c75dd691e1320baf5d29bda2ef315e360b4d042d
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-09-29 20:46:22 +0200

    Replace hello app by the flask-restless quickstart app.

--- muckr_service/__init__.py
+++ muckr_service/__init__.py
@@ -1,7 +1,33 @@
-from flask import Flask
+import flask
+import flask.ext.sqlalchemy
+import flask.ext.restless
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = flask.ext.sqlalchemy.SQLAlchemy(app)
 
-@app.route('/')
-def hello():
-    return 'Hello, world!'
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode, unique=True)
+    birth_date = db.Column(db.Date)
+
+class Computer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode, unique=True)
+    vendor = db.Column(db.Unicode)
+    purchase_time = db.Column(db.DateTime)
+    owner_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+    owner = db.relationship('Person', backref=db.backref('computers',
+                                                         lazy='dynamic'))
+
+db.create_all()
+
+manager = flask.ext.restless.APIManager(app, flask_sqlalchemy_db=db)
+
+# Create API endpoints, which will be available at /api/<tablename> by
+# default.
+manager.create_api(Person, methods=['GET', 'POST', 'DELETE'])
+manager.create_api(Computer, methods=['GET'])
+
+app.run()
