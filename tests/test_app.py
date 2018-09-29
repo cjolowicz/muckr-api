ee5f6adba4ad87b344461034f33539e352e01dbb
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-09-29 21:22:46 +0200

    Adapt test case to flask-restless example.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,18 @@
-import muckr_service
-
 import pytest
+import yaml
+
+import muckr_service
 
 
 @pytest.fixture
 def client():
     return muckr_service.app.test_client()
 
-def test_hello(client):
-    response = client.get('/')
-    assert response.data == b'Hello, world!'
+def test_person(client):
+    response = client.get('/api/person')
+    assert yaml.load(response.data) == {
+        'num_results': 0,
+        'objects': [],
+        'page': 1,
+        'total_pages': 0
+    }
