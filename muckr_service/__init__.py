9effb1cf60aa6909e98f6e864913912a056e6df6
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-09-29 21:25:22 +0200

    Avoid deprecation warning `SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead and will be disabled by default in the future'.

--- muckr_service/__init__.py
+++ muckr_service/__init__.py
@@ -5,6 +5,7 @@ import flask_restless
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = flask_sqlalchemy.SQLAlchemy(app)
 
 class Person(db.Model):
