11144f86f72939e445445ddb968e60011200bbac
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-02 15:43:37 +0200

    Minor cleanup.

--- muckr_service/__init__.py
+++ muckr_service/__init__.py
@@ -28,9 +28,12 @@ if flask_restless.__version__ == '0.17.0':
 
 def create_app():
     app = flask.Flask(__name__)
-    app.config['DEBUG'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config.from_mapping(
+        DEBUG=True,
+        SQLALCHEMY_DATABASE_URI='sqlite:////tmp/test.db',
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    )
+
     db = flask_sqlalchemy.SQLAlchemy(app)
     table_args = {
         'autoload': True,
