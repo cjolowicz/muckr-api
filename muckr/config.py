a987dd5566bb8668c0bad2e1d2bf74c22baddd7b
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-15 13:50:48 +0200

    Read SECRET_KEY from environment.

--- muckr/config.py
+++ muckr/config.py
@@ -3,5 +3,6 @@ import environs
 env = environs.Env()
 env.read_env()
 
+SECRET_KEY = env.str("SECRET_KEY")
 SQLALCHEMY_DATABASE_URI = env.str('DATABASE_URL', default='sqlite:////tmp/test.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
