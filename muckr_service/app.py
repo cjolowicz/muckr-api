8faf59758c4bda570f00b85c9a2b3e569efffc11
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-15 12:53:05 +0200

    Move imports to top of module.

--- muckr_service/app.py
+++ muckr_service/app.py
@@ -1,15 +1,15 @@
 import flask
 
 import muckr_service.config
+import muckr_service.extensions
+import muckr_service.main.routes
 
 def create_app(config_class=muckr_service.config.Config):
     app = flask.Flask(__name__)
     app.config.from_object(config_class)
 
-    import muckr_service.extensions
     muckr_service.extensions.database.init_app(app)
 
-    import muckr_service.main.routes
     app.register_blueprint(muckr_service.main.routes.blueprint)
 
     return app
