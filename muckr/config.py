23f3b7ea7adf99427ae6050d0a1065d5a4b7b9c1
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-15 14:10:18 +0200

    Require DATABASE_URL environment variable.

--- muckr/config.py
+++ muckr/config.py
@@ -3,6 +3,6 @@ import environs
 env = environs.Env()
 env.read_env()
 
-SECRET_KEY = env.str("SECRET_KEY")
-SQLALCHEMY_DATABASE_URI = env.str('DATABASE_URL', default='sqlite:////tmp/test.db')
+SECRET_KEY = env.str('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI = env.str('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
