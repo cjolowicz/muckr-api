d6ff238ae6c886e271f44f1812d3c74ebf315364
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-15 13:20:42 +0200

    Read database URL from environment or .env file.

--- muckr/config.py
+++ muckr/config.py
@@ -1,3 +1,8 @@
+import environs
+
+env = environs.Env()
+env.read_env()
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+    SQLALCHEMY_DATABASE_URI = env.str('DATABASE_URL', default='sqlite:////tmp/test.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
