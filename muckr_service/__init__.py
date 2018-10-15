83b71ee3e3d8d99be986829bd897ebc8f49b6b62
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-15 11:38:15 +0200

    Remove API endpoints. Uninstall flask-restless.

--- muckr_service/__init__.py
+++ muckr_service/__init__.py
@@ -1,23 +1,15 @@
 import flask
 import flask_sqlalchemy
-import flask_restless
 
 import muckr_service.config
 
 database = flask_sqlalchemy.SQLAlchemy()
-manager = flask_restless.APIManager()
 
 def create_app(config_class=muckr_service.config.Config):
     app = flask.Flask(__name__)
     app.config.from_object(config_class)
 
     database.init_app(app)
-    manager.init_app(app, flask_sqlalchemy_db=database)
-
-    import muckr_service.models
-
-    manager.create_api(muckr_service.models.Person, methods=['GET', 'POST', 'DELETE'])
-    manager.create_api(muckr_service.models.Computer, methods=['GET'])
 
     return app
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,8 @@
 import unittest
-import json
+import datetime
 
 import muckr_service
+import muckr_service.models
 import muckr_service.config
 
 class TestConfig(muckr_service.config.Config):
@@ -14,41 +15,20 @@ class MuckrServiceTestCase(unittest.TestCase):
         self.app_context = self.app.app_context()
         self.app_context.push()
         muckr_service.database.create_all()
-        self.client = self.app.test_client()
 
     def tearDown(self):
         muckr_service.database.session.remove()
         muckr_service.database.drop_all()
         self.app_context.pop()
 
-    def test_get_person(self):
-        response = self.client.get('/api/person')
-        assert json.loads(response.data) == {
-            'num_results': 0,
-            'objects': [],
-            'page': 1,
-            'total_pages': 0
-        }
+    def test_person(self):
+        birth_date = datetime.datetime(1970, 1, 1)
+        person = muckr_service.models.Person(
+            name='john',
+            birth_date=birth_date)
 
-    def test_post_person(self):
-        person = {
-            'name': u'Abraham Lincoln',
-            'birth_date': u'1809-02-12',
-        }
-
-        response = self.client.post(
-            '/api/person',
-            data=json.dumps(person),
-            headers={'Content-Type': 'application/json'}
-        )
-
-        assert response.status == '201 CREATED'
-        assert json.loads(response.data) == {
-            'id': 1,
-            'name': u'Abraham Lincoln',
-            'birth_date': u'1809-02-12',
-            'computers': [],
-        }
+        assert person.name == 'john'
+        assert person.birth_date == birth_date
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
