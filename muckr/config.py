fc0deb0d6e9eb10425b4bc9e17608fa098684b8d
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-16 09:33:21 +0200

    Add BCRYPT_LOG_ROUNDS configuration variable.

--- muckr/config.py
+++ muckr/config.py
@@ -3,6 +3,7 @@ import environs
 env = environs.Env()
 env.read_env()
 
+BCRYPT_LOG_ROUNDS = env.int('BCRYPT_LOG_ROUNDS', default=12)
 SECRET_KEY = env.str('SECRET_KEY')
 SQLALCHEMY_DATABASE_URI = env.str('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
