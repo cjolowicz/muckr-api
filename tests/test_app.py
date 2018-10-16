ed2c57cc1de45819c4f09eb16b5a50fcfe98cb17
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-16 21:26:59 +0200

    Move model tests to test_models.

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,28 +3,6 @@ import os
 import environs
 import pytest
 
-import muckr.extensions
-import muckr.models
-
-
-@pytest.mark.usefixtures('database')
-class TestModels:
-    def test_user(self):
-        user = muckr.models.User(
-            username='john',
-            email='john@example.com',
-            password_hash='xxxx')
-
-        assert user.id is None
-        assert user.username == 'john'
-        assert user.email == 'john@example.com'
-        assert user.password_hash == 'xxxx'
-
-        muckr.extensions.database.session.add(user)
-        muckr.extensions.database.session.commit()
-
-        assert user.id == 1
-
 
 class TestViews:
     def test_index(self, client):
