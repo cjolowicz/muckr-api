28f93d4e4e173adf9d6807e96103f9c99bcdde54
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-17 22:47:16 +0200

    Make username and email required fields in user schema.

--- muckr/user/models.py
+++ muckr/user/models.py
@@ -24,6 +24,6 @@ class User(db.Model):
 
 class UserSchema(Schema):
     id = fields.Integer(dump_only=True)
-    username = fields.Str()
-    email = fields.Email()
+    username = fields.Str(required=True)
+    email = fields.Email(required=True)
     password_hash = fields.Str(load_only=True)
