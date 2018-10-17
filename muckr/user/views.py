93e03ff751fb8e3c3c164c8e49bc738425aaae12
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-10-17 21:59:26 +0200

    Add helper function muckr.user.views.jsonify.

--- muckr/user/views.py
+++ muckr/user/views.py
@@ -6,10 +6,14 @@ from muckr.user.models import User, UserSchema
 blueprint = flask.Blueprint('user', __name__)
 
 
+def jsonify(data):
+    response = flask.jsonify(data)
+    response.mimetype = 'application/vnd.api+json'
+    return response
+
+
 @blueprint.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
     user = User.query.get_or_404(id)
     data, errors = UserSchema().dump(user)
-    response = flask.jsonify(data)
-    response.mimetype = 'application/vnd.api+json'
-    return response
+    return jsonify(data)
