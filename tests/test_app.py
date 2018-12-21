992f8f964e6bdc448d1d329a55c208c4b4666d86
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2018-12-21 22:11:07 +0100

    Add missing test for muckr.app

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,13 @@ import muckr.app
 import muckr.extensions
 
 
-def test_import(app):
+def test_import_returns_name_value_pair(app):
     attribute, value = muckr.app._import('muckr.extensions.database')
     assert attribute == 'database'
     assert value is muckr.extensions.database
+
+
+def test_register_shell_context_registers_database(app):
+    context = app.make_shell_context()
+    assert 'database' in context
+    assert context['database'] is muckr.extensions.database
