49c14053fee815071262ff081af2fa3666f28b2e
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-12-23 13:49:01 +0100

    Move artist fixtures to tests.artist.fixtures

--- tests/artist/conftest.py
+++ tests/artist/conftest.py
@@ -1,18 +1,1 @@
-'''Defines fixtures available to artist tests.'''
-import pytest
-
-from tests.artist.factories import ArtistFactory
-
-
-@pytest.fixture
-def artist(database):
-    artist = ArtistFactory.create()
-    database.session.commit()
-    return artist
-
-
-@pytest.fixture
-def artists(database):
-    artists = ArtistFactory.create_batch(25)  # fill more than 2 pages
-    database.session.commit()
-    return artists
+from tests.artist.fixtures import *  # noqa
