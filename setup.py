f64a41789a9511e8fd654b88bb0408676860ff3e
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-12-25 14:12:24 +0100

    Use double quotes in setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,34 @@
 import setuptools
 
 
-with open('VERSION') as file:
+with open("VERSION") as file:
     version = file.read()
 
-with open('README.md') as file:
+with open("README.md") as file:
     long_description = file.read()
 
-with open('requirements/base.in') as file:
+with open("requirements/base.in") as file:
     install_requires = file.read().splitlines()
 
-with open('requirements/dev.in') as file:
-    extras_require = {'dev': file.read().splitlines()}
+with open("requirements/dev.in") as file:
+    extras_require = {"dev": file.read().splitlines()}
 
 setuptools.setup(
-    name='muckr-service',
+    name="muckr-service",
     version=version,
-    author='Claudio Jolowicz',
-    author_email='mail@claudiojolowicz.com',
-    description='Web service for muckr',
+    author="Claudio Jolowicz",
+    author_email="mail@claudiojolowicz.com",
+    description="Web service for muckr",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/cjolowicz/muckr-service',
+    long_description_content_type="text/markdown",
+    url="https://github.com/cjolowicz/muckr-service",
     packages=setuptools.find_packages(),
     install_requires=install_requires,
     extras_require=extras_require,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
     ],
 )
