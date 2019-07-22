c7ffd76bd865822613ff598a72c7ce660ea22fb4
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-22 23:59:54 +0200

    [tests] Add venue factory

---
+++ tests/venue/factories.py
@@ -0,0 +1,20 @@
+"""Factories to help in venue tests."""
+from factory import Sequence, SubFactory
+
+from muckr.venue.models import Venue
+from tests.factories import BaseFactory
+from tests.user.factories import UserFactory
+
+
+class VenueFactory(BaseFactory):
+    """Venue factory."""
+
+    name = Sequence(lambda n: "venue{0}".format(n))
+    city = Sequence(lambda n: "city{0}".format(n))
+    country = Sequence(lambda n: "country{0}".format(n))
+    user = SubFactory(UserFactory)
+
+    class Meta:
+        """Factory configuration."""
+
+        model = Venue
