db7eee07acf2ddde50084b0e477802fba116286a
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-08-02 19:22:54 +0200

    [tests] Add test for client command

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,3 +1,5 @@
+import pytest
+
 import muckr.commands
 from muckr.user.models import User
 
@@ -7,3 +9,14 @@ def test_create_admin_inserts_admin_user(app, database):
     result = runner.invoke(muckr.commands.create_admin, catch_exceptions=False)
     assert result.exit_code == 0
     assert "admin" in {user.username for user in User.query.all()}
+
+
+@pytest.mark.parametrize(
+    "url", ["https://muckr-service.herokuapp.com/artists", "https://localhost/artists"]
+)
+def test_client_succeeds(app, mocker, url):
+    mocker.patch("subprocess.run").return_value.stdout = '{"ADMIN_PASSWORD": ""}'
+    mocker.patch("requests.post").return_value.json.return_value = {"token": ""}
+    runner = app.test_cli_runner()
+    result = runner.invoke(muckr.commands.client, ["GET", url], catch_exceptions=False)
+    assert result.exit_code == 0
