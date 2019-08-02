682f483d4cedc5d8f69a33ecae0e27d4f717676b
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-08-02 19:14:26 +0200

    [commands] Add client command

--- src/muckr/commands.py
+++ src/muckr/commands.py
@@ -1,7 +1,12 @@
 """Click commands."""
+import json
+import subprocess
+import urllib.parse
+
 import click
 import flask
 import flask.cli
+import requests
 
 from muckr.user.models import User
 from muckr.extensions import database
@@ -18,3 +23,38 @@ def create_admin():
     user.set_password(config["ADMIN_PASSWORD"])
     database.session.add(user)
     database.session.commit()
+
+
+def _get_admin_credentials(url):
+    if "heroku" in url:
+        process = subprocess.run(
+            ["heroku", "config", "--json", "--app=muckr-service"], capture_output=True
+        )
+        config = json.loads(process.stdout)
+    else:
+        config = flask.current_app.config
+    username = config.get("ADMIN_USERNAME", "admin")
+    password = config["ADMIN_PASSWORD"]
+    return username, password
+
+
+def _get_baseurl(url):
+    url = urllib.parse.urlsplit(url)
+    return urllib.parse.urlunsplit([url.scheme, url.netloc, "", "", ""])
+
+
+def _get_token(baseurl, auth):
+    response = requests.post(f"{baseurl}/tokens", auth=auth)
+    return response.json()["token"]
+
+
+@click.command()
+@click.argument("method")
+@click.argument("url")
+@click.argument("args", nargs=-1)
+@flask.cli.with_appcontext
+def client(method, url, args):
+    """Simple HTTP client."""
+    token = _get_token(_get_baseurl(url), _get_admin_credentials(url))
+    args += (f"Authorization: Bearer {token}",)
+    subprocess.run(["http", "--print=b", method, url, *args])
