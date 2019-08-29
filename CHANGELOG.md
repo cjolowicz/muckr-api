b99c1fc55becf6f8f9ab3a0e2cb6142a04492419
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-08-29 14:46:43 +0200

    Use poetry (#34)
    * [poetry] Add pyproject.toml
    
    * [poetry] Add poetry.lock
    
    * [git] Ignore /dist/
    
    * [docs] Add poetry to CONTRIBUTING.md
    
    * [nox] Adapt to poetry
    
    * [docker] Adapt to poetry
    
    * [travis] Install poetry
    
    * [heroku] Inherit BUILDPACK_URL from parent app
    
    * [setup] Remove obsolete setup.py
    
    * [setup] Remove obsolete setup.cfg
    
    * [requirements] Remove obsolete requirements files
    
    * Update CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Changed
+- Use [poetry](https://poetry.eustace.io/) build backend
+
 ## [0.5.0] - 2019-08-04
 ### Changed
 - Rename project to muckr-api ([#30](../../pull/30))
--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,9 @@
-import os
-
 import nox
 
 
 nox.options.sessions = "lint", "tests"
 
-locations = "migrations", "noxfile.py", "setup.py", "src", "tests", "wsgi.py"
+locations = "migrations", "noxfile.py", "src", "tests", "wsgi.py"
 
 
 @nox.session(python="3.7")
@@ -26,30 +24,6 @@ def lint(session):
 @nox.session(python="3.7")
 def tests(session):
     """Run the test suite."""
-    package = os.path.join(
-        session.virtualenv.location,
-        "lib",
-        session.virtualenv._resolved_interpreter,
-        "site-packages",
-        "muckr_api",
-    )
-    session.install("-r", "requirements/base.txt")
-    session.install("-r", "requirements/tests.txt")
-    session.install(".")
+    session.run("poetry", "install", external=True)
     tests = session.posargs or ["tests/"]
-    session.run("pytest", f"--cov={package}", *tests)
-
-
-@nox.session(python="3.7")
-def upgrade(session):
-    """Upgrade the requirements."""
-    session.install("pip-tools")
-    for requirements in ["base", "dev", "tests"]:
-        basename = os.path.join("requirements", requirements)
-        session.run(
-            "pip-compile",
-            "--verbose",
-            "--upgrade",
-            f"--output-file={basename}.txt",
-            f"{basename}.in",
-        )
+    session.run("pytest", f"--cov=muckr_api", *tests)
